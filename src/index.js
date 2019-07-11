cc4798fdc79f605bdf1d8ef816db14689ae453bc
gc18773375617 <654992676@qq.com>
2019-07-11 11:04:52 +0800

    优化加载

--- src/index.js
+++ src/index.js
@@ -2,9 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+let tiger;
+tiger = {
+    num:10000
+}
+const increase = {
+    type:1
+};
+const decrease = {
+    type:2
+};
 
+const reducer = (state = tiger, action) => {
+    switch (action.type){
+      case 1: 
+        return state.num += 100;
+      case 2: 
+        return state.num -= 100;
+      default: 
+        return state;
+    }
+}
+const store = createStore(reducer);
+store.subscribe(() =>
+  console.log(store.getState())
+)
+console.log(store.dispatch(increase))
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
